1b3b08eb7c0e89c77ae7a9a300e6da45cddb0f09
d4onda <122915123+d4onda@users.noreply.github.com>
2024-10-04 10:53:49 -0700

    5th Commit: SmallMistake, No Backend yet

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import Table from "./Table";
-import Form from "./Form";
 
 const characters = [
   {
@@ -22,17 +21,29 @@ const characters = [
 ];
 
 function MyApp() {
-  const [characters, setCharacters] = useState([]);
-
+  const [characters, setCharacters] = useState( [
+    {
+      name: "Charlie",
+      job: "Janitor"
+    },
+    {
+      name: "Mac",
+      job: "Bouncer"
+    },
+    {
+      name: "Dee",
+      job: "Aspring actress"
+    },
+    {
+      name: "Dennis",
+      job: "Bartender"
+    }
+  ]);
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
       return i !== index;
     });
     setCharacters(updated);
-  }
-
-  function updateList(person) {
-    setCharacters([...characters, person]);
   }
     return (
       <div className="container">
@@ -40,7 +51,6 @@ function MyApp() {
         characterData={characters}
         removeCharacter={removeOneCharacter}
         />
-        <Form handleSubmit={updateList} />
       </div>
     );
   }
